c17ad6f69ac62c8014ee87081162df519f2ee046
Malcolm <malcolmivy7@gmail.com>
2019-03-12 23:05:23 -0500

    Re-Do of B3( i hope this is correct)

--- app/src/main/java/com/malcolm/say/MainActivity.kt
+++ app/src/main/java/com/malcolm/say/MainActivity.kt
@@ -9,10 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     var count = 0
 
-    myButton.setOnClickListener {
-        counter++
-        myText.text = resources.getQuantityString(R.string. was_clicked , counter, counter)
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         val myText: TextView = findViewById(R.id.my_text_view)
         val myButton: Button = findViewById(R.id.my_button)
         myButton.setOnClickListener {
-            myText.text = getString(R.string.was_clicked)
+            counter+=1
+            myText.text =resources.getQuantityString(R.plurals.was_clicked, counter, counter)
+        }
         }
 
 
