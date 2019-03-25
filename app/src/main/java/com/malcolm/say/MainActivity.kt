bf4ec6608d89ac4e0793f5634270e192601912eb
Malcolm <malcolmivy7@gmail.com>
2019-03-25 18:47:43 -0500

    Yea this is the error thing forthe assignment

--- app/src/main/java/com/malcolm/say/MainActivity.kt
+++ app/src/main/java/com/malcolm/say/MainActivity.kt
@@ -2,12 +2,19 @@ package com.malcolm.say
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onMapReady(p0: GoogleMap?) {
+        Log.i ("Say","Map is ready")
+    }
 
-    var count = 0
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,14 +23,11 @@ class MainActivity : AppCompatActivity() {
         val myText: TextView = findViewById(R.id.my_text_view)
         val myButton: Button = findViewById(R.id.my_button)
         myButton.setOnClickListener {
-            counter+=1
-            myText.text =resources.getQuantityString(R.plurals.was_clicked, counter, counter)
+            counter += 1
+            myText.text = resources.getQuantityString(R.plurals.was_clicked, counter, counter)
         }
-        }
 
-
-
-
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.my_map_view) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
-
-
+}
