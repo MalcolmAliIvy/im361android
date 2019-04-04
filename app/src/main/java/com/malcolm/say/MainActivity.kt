b21aa0b7e3a6e4e32e7307ec3b45cf688f024c6d
Malcolm <malcolmivy7@gmail.com>
2019-04-03 23:42:51 -0500

    this is the one without errors

--- app/src/main/java/com/malcolm/say/MainActivity.kt
+++ app/src/main/java/com/malcolm/say/MainActivity.kt
@@ -1,38 +1,19 @@
 package com.malcolm.say
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.TextView
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    override fun onMapReady(map: GoogleMap?) {
-        val morton = LatLng(40.6091693, -89.5392298)
-//        map?.addMarker(MarkerOptions().position(morton).title("Pumpkin capital of the world"))
-//        map?.moveCamera(CameraUpdateFactory.newLatLng(morton))
-
-        Log.i ("Say","Map is ready")
-    }
-
-    var counter = 0
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myText: TextView = findViewById(R.id.my_text_view)
-        val myButton: Button = findViewById(R.id.my_button)
+        val myButton: Button = findViewById(R.id.start_button)
         myButton.setOnClickListener {
-            counter += 1
-            myText.text = resources.getQuantityString(R.plurals.was_clicked, counter, counter)
+            val intent = Intent(this, FrenchActivity::class.java)
+            startActivity(intent)
         }
-
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.my_map_view) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
 }
--- app/src/main/java/com/malcolm/say/TestActivity.kt
+++ app/src/main/java/com/malcolm/say/TestActivity.kt
@@ -3,7 +3,7 @@ package com.malcolm.say
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class testactivities : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_french)
